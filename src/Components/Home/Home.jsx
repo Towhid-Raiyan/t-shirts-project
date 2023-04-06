4ca4db2b897911f4545563eea40d4c36d4d4620e
Towhid-Raiyan <towhid.raiyan.com>
2023-04-07 02:00:37 +0600

    cart product added and remove functioanlity and toast added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import Tshirt from '../Tshirt/Tshirt';
+import './Home.css';
+import toast from 'react-hot-toast';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        const exist = cart.find(ts => ts._id === tshirt._id);
+        if (exist) {
+            toast('You already added this product');
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+    }
+
+    const handleRemoveFromCart = id => {
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining);
+    }
     return (
-        <div>
-            <h3>This is from home: {tshirts.length}</h3>
+        <div className='home-container'>
+            <div className="t-shirt-container">
+                {
+                    tshirts.map(tshirt => <Tshirt
+                        id={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
+            </div>
         </div>
     );
 };
